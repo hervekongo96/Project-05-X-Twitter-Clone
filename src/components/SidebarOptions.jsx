ffb0c462768dc32708c78e5def60ce47398c3b78
hervekongo96 <hervekongo.hk@gmail.com>
2023-12-05 10:05:40 +0100

    ajout du code css de sidebaroptions

--- src/components/SidebarOptions.jsx
+++ src/components/SidebarOptions.jsx
@@ -1,7 +1,33 @@
-export default function SidebarOptions({text, Icon, active}) {
+import { NavLink } from "react-router-dom"
+
+export default function SidebarOptions() {
   return (
-    <div className="sidebarOption">
-        {text}
-    </div>
+    <ul className="sidebarOptions">
+        <li>
+            <a to="/">Home</a>
+        </li>
+        <li>
+            <a to="/">Explore</a>
+        </li>
+        <li>
+            <a to="/">Notifications</a>
+        </li>
+        <li>
+            <a to="/">Messages</a>
+        </li>
+        <li>
+            <a to="/">Bookmarks</a>
+        </li>
+        <li>
+            <a to="/">Lists</a>
+        </li>
+        <li>
+            <a to="/">Profile</a>
+        </li>
+        <li>
+            <a to="/">More</a>
+        </li>
+    </ul>
   )
 }
+
