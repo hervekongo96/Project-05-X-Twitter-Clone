7bbcdf5259b3f2b574641e0a343d4e3b8e5d8268
hervekongo96 <hervekongo.hk@gmail.com>
2023-12-05 10:05:40 +0100

    ajout du code css de sidebaroptions

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ export default function Sidebar() {
   return (
     <div className='sidebar'>
         <h2>Icon</h2>
-        <SidebarOptions text={"Home"} />
+        <SidebarOptions />
     </div>
   )
 }
