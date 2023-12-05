09108aa4fc9a77b492a7cc3413ac7eeafc31553c
hervekongo96 <hervekongo.hk@gmail.com>
2023-12-05 10:30:39 +0100

    ajout des icons sur les sidebarOptions

--- src/components/SidebarOptions.jsx
+++ src/components/SidebarOptions.jsx
@@ -1,31 +1,39 @@
 import { NavLink } from "react-router-dom"
+import Home from "../images/Home-Fill.svg"
+import Explore from "../images/Explore.svg"
+import Notifications from "../images/Notifications.svg"
+import Messages from "../images/Messages.svg"
+import Bookmarks from "../images/Bookmarks.svg"
+import Lists from "../images/Lists.svg"
+import Profile from "../images/Profile.svg"
+import More from "../images/More.svg"
 
 export default function SidebarOptions() {
   return (
     <ul className="sidebarOptions">
         <li>
-            <a to="/">Home</a>
+            <img src={Home} alt="home" /><a to="/">Home</a>
         </li>
         <li>
-            <a to="/">Explore</a>
+            <img src={Explore} alt="explore" /><a to="/">Explore</a>
         </li>
         <li>
-            <a to="/">Notifications</a>
+            <img src={Notifications} alt="notification" /><a to="/">Notifications</a>
         </li>
         <li>
-            <a to="/">Messages</a>
+            <img src={Messages} alt="message" /><a to="/">Messages</a>
         </li>
         <li>
-            <a to="/">Bookmarks</a>
+            <img src={Bookmarks} alt="bookmark" /><a to="/">Bookmarks</a>
         </li>
         <li>
-            <a to="/">Lists</a>
+            <img src={Lists} alt="Lists" /><a to="/">Lists</a>
         </li>
         <li>
-            <a to="/">Profile</a>
+            <img src={Profile} alt="Profile" /><a to="/">Profile</a>
         </li>
         <li>
-            <a to="/">More</a>
+            <img src={More} alt="More" /><a to="/">More</a>
         </li>
     </ul>
   )
