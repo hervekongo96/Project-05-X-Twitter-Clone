46f2d4d528c4e47ae6eae49f14171bdd0fd0a139
hervekongo96 <hervekongo.hk@gmail.com>
2023-12-09 00:04:47 +0100

    correction de redirection de page profil sur avar

--- src/components/Sidebar/Navigation.jsx
+++ src/components/Sidebar/Navigation.jsx
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom"
 
 function Navigation({src, url, texte, className}) {
   return (
-    <NavLink to={url} className={className}>
+      <NavLink to={url} className={className}>
         <img src={src} />
         <p>{texte}</p>
-    </NavLink>
+      </NavLink>
   )
 }
 
