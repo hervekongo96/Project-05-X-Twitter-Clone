e23529a7a4837fdd85675ea11d4eb93ee4cc9228
hervekongo96 <hervekongo.hk@gmail.com>
2023-12-08 16:50:34 +0100

    configuration de button like, suppression de la composant tweetbuttonAction et restruturation

--- src/components/TweetEditorButtons.jsx
+++ src/components/TweetEditorButtons.jsx
@@ -6,11 +6,11 @@ export default function TweetEditorButtons() {
   return (
     <div className="tweet-editor-buttons">
       <div className="tweet-editor-actions">
-      <TweetEditorActions url={"src/images/Media.svg"} />
-      <TweetEditorActions url={"src/images/Gif.svg"} />
-      <TweetEditorActions url={"src/images/Poll.svg"} />
-      <TweetEditorActions url={"src/images/Group.svg"} />
-      <TweetEditorActions url={"src/images/Schedule.svg"} />
+        <TweetEditorActions url={"src/images/Media.svg"} />
+        <TweetEditorActions url={"src/images/Gif.svg"} />
+        <TweetEditorActions url={"src/images/Poll.svg"} />
+        <TweetEditorActions url={"src/images/Group.svg"} />
+        <TweetEditorActions url={"src/images/Schedule.svg"} />
       </div>
         <Button value={"Tweet"} className={"button"}/>
     </div>
