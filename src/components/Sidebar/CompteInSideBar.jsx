d3c924e6ea26893fee9580d89d8ee0aacbe914a3
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-13 23:01:15 +0100

    utilisation d'initial data dans ule profil utilisateur current

--- src/components/Sidebar/CompteInSideBar.jsx
+++ src/components/Sidebar/CompteInSideBar.jsx
@@ -1,17 +1,23 @@
 import Avatar from "../TimeLine/Avatar"
 import More2 from "/images/More-2.svg"
+import user from "../../Data/initial-data.json"
+
 
 export default function CompteInSideBar() {
   return (
-    <div className='compte-in-sidebar'>
-        <div className="compte-in-sidebar-avatar"><Avatar /></div>
-        <div>
-            <div className="compte-icon">
-                <p>Bradly Ortiz</p>
+    <>
+      {
+         <div className='compte-in-sidebar'>
+            <div className="compte-in-sidebar-avatar"><Avatar src={user.currentuser.profil} url={"/profil"}/></div>
+            <div>
+              <div className="compte-icon">
+                <p>{user.currentuser.name}</p>
                 <img src={More2} alt="" />
+              </div>
+              <span className="tweet-title-details">{user.currentuser.subname}</span>
             </div>
-            <span className="tweet-title-details">@bradly</span>
-        </div>
-    </div>
+          </div>
+      }
+    </>
   )
 }
