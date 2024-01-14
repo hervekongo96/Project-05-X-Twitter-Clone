57432d085405d3302004699d700639ae4541c1b3
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-14 01:04:23 +0100

    changement d'affichage de profil user current

--- src/components/Tweet/TweetEditorForm.jsx
+++ src/components/Tweet/TweetEditorForm.jsx
@@ -2,13 +2,15 @@ import TweetEditorButtons from "./TweetEditorButtons"
 
 
 export default function TweetEditorForm() {
+  
   return (
-    <div className="tweet-editor-form">
+    <div className="tweet-editor-form" id="tweetEditor">
         <form method="" action="">
             <input 
                 type="text" 
                 placeholder="What's happening ?" 
                 className="tweet-editor-input" 
+                id="tweetpost"
             />
             <TweetEditorButtons />
         </form>
