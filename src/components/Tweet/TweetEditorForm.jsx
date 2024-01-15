800a5578d2d21882732f16bce5a7647b6256e26d
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-15 11:26:16 +0100

    implementation d'envoi de nouveau post

--- src/components/Tweet/TweetEditorForm.jsx
+++ src/components/Tweet/TweetEditorForm.jsx
@@ -1,14 +1,37 @@
 import TweetEditorButtons from "./TweetEditorButtons"
+import { useState } from "react"
+import data from "../../Data/initial-data.json"
 
 
 export default function TweetEditorForm() {
+
+
+  const [post, setPost] = useState("");
+  const [dataList, setDataList] = useState([]);;
+
+  const handleInputChange = (e) => {
+    setPost(e.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newPost = {
+      id: Date.now(),
+      post: post
+    }
+    setDataList([...dataList, newPost]);
+    setPost('')
+    console.log(newPost)
+  }
   return (
     <div className="tweet-editor-form" id="tweetEditor">
-        <form >
+        <form  onSubmit={handleSubmit}>
             <input 
                 type="text" 
                 placeholder="What's happening ?" 
-                className="tweet-editor-input" 
+                className="tweet-editor-input"
+                value={post}
+                onChange={handleInputChange} 
             />
             <TweetEditorButtons />
         </form>
