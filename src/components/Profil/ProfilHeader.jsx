8b0a2a1007ee9f5c2378f032de533d4596a78e67
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-16 18:12:49 +0100

    utilisation de data venant de useContext dans profil user currents

--- src/components/Profil/ProfilHeader.jsx
+++ src/components/Profil/ProfilHeader.jsx
@@ -2,22 +2,26 @@ import PageTitle from "../Header/PageTitle"
 import ProfilImage from "/images/profile-photo.png"
 import Tweet from "../Tweet/Tweet"
 import ProfilNavigation from "./ProfilNavigation"
+import { useContext } from "react"
+import UserContext from "../../UserContext"
 
 
 function Info() {
+
+  const user = useContext(UserContext)
   return (
     <div className="timeline">
         <div className="header">
             <PageTitle texte={"Profil"} />
         </div>
         <div className="image-background-profil">
-          <img src={ProfilImage} alt="" />
+          <img src={user.profil} alt="" />
         </div>
         <div className="profil-news">
           <button className="profil-button">Edit profile</button>
           <div className="profil-hedear-title">
-            <h3>Bradly Ortiz</h3>
-            <span className="tweet-title-details">@badly</span>
+            <h3>{user.name}</h3>
+            <span className="tweet-title-details">{user.subname}</span>
           </div>
           <p>President Joe Biden a new agreement reached with the European union to ease Trume era tariffs on aluminum and steel as a 'major breakthough' that would service to both strengthen the US</p>
           <div className="profil-follow">
