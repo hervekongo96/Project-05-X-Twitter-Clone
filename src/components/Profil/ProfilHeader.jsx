8cb6de0076e77aa7619987ad515bbb121767e644
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-17 00:05:13 +0100

    filtrage de post de chaque user et amelioration de l'affichage de page user et profil

--- src/components/Profil/ProfilHeader.jsx
+++ src/components/Profil/ProfilHeader.jsx
@@ -1,18 +1,31 @@
 import PageTitle from "../Header/PageTitle"
-import ProfilImage from "/images/profile-photo.png"
-import Tweet from "../Tweet/Tweet"
 import ProfilNavigation from "./ProfilNavigation"
 import { useContext } from "react"
 import UserContext from "../../UserContext"
-
+import { Link } from "react-router-dom"
+import BackButton from "../backButton"
+import data from '../../Data/initial-data.json'
+import IconComent from "../TweetActionButton/comentIcon/IconComent"
+import IconLike from "../TweetActionButton/like/IconLike"
+import IconRetweet from "../TweetActionButton/retweetIcon/IconRetweet"
+import IconUpload from "../TweetActionButton/uploadIcon/IconUpload"
 
 function Info() {
 
   const user = useContext(UserContext)
+  const filtrer = data.tweet.filter(filtre => filtre.auteur == user.name)
+  
   return (
     <div className="timeline">
         <div className="header">
-            <PageTitle texte={"Profil"} />
+          <span className='back-header'>
+            <span className="back-button">
+              <Link to={'/'}><BackButton /></Link>
+            </span>
+              <h1>
+                <PageTitle texte={user.name} />
+              </h1>
+          </span>
         </div>
         <div className="image-background-profil">
           <img src={user.profil} alt="" />
@@ -32,7 +45,34 @@ function Info() {
               <ProfilNavigation />
           </div>
         </div>
-      <Tweet />  
+        {
+          filtrer.map(posts => (
+            <div className="tweet">
+              <div className="profil-hedear-title">
+                <div className="tweet-content">
+                  <div className="tweet-body">
+                    <div className="tweet-title">
+                      <span className='tweet-title-author'>{posts.auteur}</span>
+                        {posts.certificat ? (<span><img src="/images/Vector.svg" alt="certificated" /></span>):(<span>-</span>)}
+                        <span className='tweet-title-details'>{posts.detailsTitleTweet}</span>
+                        <span className='tweet-title-details'>{posts.time}</span>
+                    </div>
+                    <div className="tweet-text">
+                      {posts.textTweet}
+                    </div>
+                    {posts.imageTweet && <span className='tweet-image'><img src={posts.imageTweet} alt=""/></span>}
+                  </div>
+                  <div className="tweet-actions">
+                    <IconComent counts={posts.message} className={'tweet-action-button-reply'}/>
+                    <IconRetweet counts={posts.share} className={'tweet-action-button-retweet'}/>
+                    <IconLike counts={posts.like} className={'tweet-action-button-react'} />
+                    <IconUpload counts={posts.upload} className={'tweet-action-button-group'}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        }
     </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 import TimeLine from '../components/TimeLine/TimeLine';
 import Trends from '../components/Trends/Trends';
 import Sidebar from '../components/Sidebar/Sidebar';
-// import { UserProvider } from '../UserContext';
-// import UserProfile from '../Data/initial-data.json';
-
-// const user = {
-//   profil  : UserProfile.currentuser.profil,
-//   name    : UserProfile.currentuser.name,
-//   subname : UserProfile.currentuser.subname
-// };
 
 
 function Home() {
