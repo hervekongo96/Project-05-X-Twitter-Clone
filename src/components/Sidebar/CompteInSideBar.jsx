3d749b97b3a53736479543e1452590ab2f3333f1
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-16 12:44:39 +0100

    creation de useContext et utilisation au niveau de compteInsidbar

--- src/components/Sidebar/CompteInSideBar.jsx
+++ src/components/Sidebar/CompteInSideBar.jsx
@@ -1,20 +1,24 @@
+import { useContext } from "react";
 import Avatar from "../TimeLine/Avatar"
 import More2 from "/images/More-2.svg"
-import user from "../../Data/initial-data.json"
+//import user from "../../Data/initial-data.json"
+import UserContext from "../../UserContext"
 
 
 export default function CompteInSideBar() {
+
+  const user = useContext(UserContext);
   return (
     <>
       {
          <div className='compte-in-sidebar'>
-            <div className="compte-in-sidebar-avatar"><Avatar src={user.currentuser.profil} url={"/profil"}/></div>
+            <div className="compte-in-sidebar-avatar"><Avatar src={user.profil} url={"/profil"}/></div>
             <div>
               <div className="compte-icon">
-                <p>{user.currentuser.name}</p>
+                <p>{user.name}</p>
                 <img src={More2} alt="" />
               </div>
-              <span className="tweet-title-details">{user.currentuser.subname}</span>
+              <span className="tweet-title-details">{user.subname}</span>
             </div>
           </div>
       }
