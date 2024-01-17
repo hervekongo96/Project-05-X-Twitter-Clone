92a03a0d7fe32a2bd35b393cdf786787130a2d3a
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-17 23:47:08 +0100

    creation de likecontext

--- src/components/TweetActionButton/like/IconLike.jsx
+++ src/components/TweetActionButton/like/IconLike.jsx
@@ -25,20 +25,42 @@ const handleClick = () =>{
     }
 }
 
-const divStyles = {
-    color: !icon ? 'red' : ' ',
-    padding: '10px',
-}
-
   return (
     <div className="tweet-action-button-react" title="like">
         <span className="tweet-action-button-over">
             <img onClick={()=>{changeIcon(), handleClick()}} src={`${icon ? reacts : liked}`} />
         </span>
-        <span style={divStyles}>{count}</span>
+        <span style={{ color: !icon ? 'red' : ' ', padding: '10px' }}>{count}</span>
     </div>
   )
 }
+
+// import { useContext } from "react"
+// import { LikeContext } from "../../../LikeContext"
+
+
+// const reacts = "/images/React.svg"
+// const liked = "/images/Liked.svg"
+
+
+
+// export default function IconLike() {
+
+//     const { icon, count, changeIcon, handleClick } = useContext(LikeContext)
+
+//   return (
+//     <div className="tweet-action-button-react" title="like">
+//         <span className="tweet-action-button-over">
+//             <img onClick={() => { changeIcon(); handleClick(); }} src={`${icon ? reacts : liked}`} />
+//         </span>
+//         <span style={{ color: !icon ? 'red' : ' ', padding: '10px' }}>{count}</span>
+//     </div>
+//   )
+// }
+
+
+
+
 
 
 
