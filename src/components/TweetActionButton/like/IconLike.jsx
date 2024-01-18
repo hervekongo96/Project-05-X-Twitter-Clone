0912b223dafc0967dac82cbef9b6e7b198d4a826
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-18 10:00:36 +0100

    suppression de profil photo

--- src/components/TweetActionButton/like/IconLike.jsx
+++ src/components/TweetActionButton/like/IconLike.jsx
@@ -1,42 +1,4 @@
-// import { useState } from "react"
-
-
-// const reacts = "/images/React.svg"
-// const liked = "/images/Liked.svg"
-
-
-
-// export default function IconLike({counts}) {
-
-// const [icon, setIcon] = useState(true)
-// const [count, setCount] = useState(counts)
-
-
-
-// const changeIcon =() =>{
-//     setIcon(!icon)
-// }
-
-// const handleClick = () =>{
-//     if(!icon){
-//         setCount(count -1)
-//     }else{
-//         setCount(count +1)
-//     }
-// }
-
-//   return (
-//     <div className="tweet-action-button-react" title="like">
-//         <span className="tweet-action-button-over">
-//             <img onClick={()=>{changeIcon(), handleClick()}} src={`${icon ? reacts : liked}`} />
-//         </span>
-//         <span style={{ color: !icon ? 'red' : ' ', padding: '10px' }}>{count}</span>
-//     </div>
-//   )
-// }
-
-import { useContext } from "react"
-import { LikeContext } from "../../../LikeContext"
+import { useState } from "react"
 
 
 const reacts = "/images/React.svg"
@@ -44,19 +6,57 @@ const liked = "/images/Liked.svg"
 
 
 
-export default function IconLike() {
+export default function IconLike({counts}) {
 
-    const { icon, count, changeIcon, handleClick } = useContext(LikeContext)
+const [icon, setIcon] = useState(true)
+const [count, setCount] = useState(counts)
+
+
+
+const changeIcon =() =>{
+    setIcon(!icon)
+}
+
+const handleClick = () =>{
+    if(!icon){
+        setCount(count -1)
+    }else{
+        setCount(count +1)
+    }
+}
 
   return (
     <div className="tweet-action-button-react" title="like">
         <span className="tweet-action-button-over">
-            <img onClick={() => { changeIcon(); handleClick(); }} src={`${icon ? reacts : liked}`} />
+            <img onClick={()=>{changeIcon(), handleClick()}} src={`${icon ? reacts : liked}`} />
         </span>
         <span style={{ color: !icon ? 'red' : ' ', padding: '10px' }}>{count}</span>
     </div>
   )
 }
+
+// import { useContext } from "react"
+// import { LikeContext } from "../../../LikeContext"
+
+
+// const reacts = "/images/React.svg"
+// const liked = "/images/Liked.svg"
+
+
+
+// export default function IconLike() {
+
+//     const { icon, count, changeIcon, handleClick } = useContext(LikeContext)
+
+//   return (
+//     <div className="tweet-action-button-react" title="like">
+//         <span className="tweet-action-button-over">
+//             <img onClick={() => { changeIcon(); handleClick(); }} src={`${icon ? reacts : liked}`} />
+//         </span>
+//         <span style={{ color: !icon ? 'red' : ' ', padding: '10px' }}>{count}</span>
+//     </div>
+//   )
+// }
 
 
 
