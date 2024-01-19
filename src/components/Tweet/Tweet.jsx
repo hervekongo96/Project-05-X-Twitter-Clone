46ee82983e024f3494fb8994909b5112b09d9ca5
hervekongo96 <hervekongo.hk@gmail.com>
2024-01-19 22:03:15 +0100

    utilisation de classe tailwind dans le composant tweet

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -15,26 +15,26 @@ export default function Tweet() {
     <>
         {
             tweetData.map(tweet =>(
-                <div className='tweet' key={tweet.id}>
+                <div className='flex justify-start items-start gap-5 p-6 tweet' key={tweet.id}>
                     <div className="tweet-avatar">
                     <Link to={`/profils/${tweet.auteur}`}>
                         <img src={tweet.avatarTweet} alt="" />
                     </Link>
                     </div>
-                    <div className="tweet-content">
-                        <div className="tweet-body">
-                            <div className="tweet-title">
+                    <div className="flex flex-col items-start justify-start gap-8 w-full">
+                        <div className="flex flex-col gap-2">
+                            <div className="flex justify-start items-start gap-2 text-base">
                                 <span className='tweet-title-author'>{tweet.auteur}</span>
                                 {tweet.certificat ? (<span><img src="/images/Vector.svg" alt="certificated" /></span>):(<span>-</span>)}
-                                <span className='tweet-title-details'>{tweet.detailsTitleTweet}</span>
-                                <span className='tweet-title-details'>{tweet.time}</span>
+                                <span className='text-gray-600'>{tweet.detailsTitleTweet}</span>
+                                <span className='text-gray-600'>{tweet.time}</span>
                             </div>
-                            <div className="tweet-text">
+                            <div className="text-gray-300 text-base">
                                 {tweet.textTweet}
                             </div>
-                            {tweet.imageTweet && <span className='tweet-image'><img src={tweet.imageTweet} alt=""/></span>}
+                            {tweet.imageTweet && <img src={tweet.imageTweet} alt="" className='max-w-full h-auto rounded-3xl border-2 border-gray-700'/>}
                         </div>
-                        <div className="tweet-actions">
+                        <div className="flex justify-center items-center gap-20 text-gray-500 text-base">
                             <IconComent counts={tweet.message} className={'tweet-action-button-reply'}/>
                             <IconRetweet counts={tweet.share} className={'tweet-action-button-retweet'}/>
                             <IconLike counts={tweet.like} className={'tweet-action-button-react'} />
